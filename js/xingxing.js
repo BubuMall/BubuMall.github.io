f864a8af95a7dff5f9bb0e48d539d814360ae406
whatqiu <1161759361@qq.com>
2023-03-13 23:26:33 +0800

    update day22

--- js/xingxing.js
+++ js/xingxing.js
@@ -1,30 +1,67 @@
-// var $ = require( "jquery" );
-// 动态心跳
-$(document).ready(function(e){
-    $('.copyright').html('©2023 <i class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> ');
-})
-
-$(document).ready(function(e){
-    show_date_time();
-})
-
-//本站运行时间
-function show_date_time(){
-$('.framework-info').html('本站已运行<span id="span_dt_dt" style="color: #fff;"></span>');
-window.setTimeout("show_date_time()", 1000);
-BirthDay=new Date("2/06/2023 0:0:0");
-today=new Date();
-timeold=(today.getTime()-BirthDay.getTime());
-sectimeold=timeold/1000
-secondsold=Math.floor(sectimeold);
-msPerDay=24*60*60*1000
-e_daysold=timeold/msPerDay
-daysold=Math.floor(e_daysold);
-e_hrsold=(e_daysold-daysold)*24;
-hrsold=Math.floor(e_hrsold);
-e_minsold=(e_hrsold-hrsold)*60;
-minsold=Math.floor((e_hrsold-hrsold)*60);
-seconds=Math.floor((e_minsold-minsold)*60);
-span_dt_dt.innerHTML='<font style=color:#afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
-}
-
+$(document).ready(function () {
+    // 获取音频对象
+    const audioElement = $("<audio>")
+      .addClass("nahida")
+      .css("display", "none")
+      .append(
+        $("<source>")
+          .attr(
+            "src",
+            "./sound/nahida_chat.mp3"
+          )
+          .attr("type", "audio/mpeg")
+      );
+    // .insertAfter('header');
+    $("header").after(audioElement);
+  
+    var audio = $(".nahida")[0];
+  
+    // 定义音频链接数组和索引
+    var audioLinks = [
+      "./sound/nahida_init.mp3",
+      "./sound/nahida_chat.mp3",
+      "./sound/nahida_wait.mp3",
+    ];
+    var audioIndex = 0;
+  
+    // 点击 header 播放/暂停音频
+    $("header").on("click", function () {
+      if (audio.paused) {
+        audio.play(); // 播放音频
+        audio.addEventListener("ended", (e) => {
+          // 监听音频播放完毕事件，播放下一个音频链接
+          playNext();
+        });
+      } else {
+        audio.pause(); // 暂停音频
+      }
+    });
+  
+    // 定义播放下一个音频链接函数
+    function playNext() {
+      // 索引加 1，并对链接数量取余，实现循环播放
+      audioIndex = (audioIndex + 1) % audioLinks.length;
+  
+      // 切换到下一个音频链接，并自动播放
+      audio.src = audioLinks[audioIndex];
+      // audio.play();
+    }
+  
+    // 绑定鼠标移动事件，计时5分钟后重新播放音频
+    var timeout = null;
+    if (audio.playing) {
+      return;
+    } else {
+      $(document).on("mousemove", function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+          audio.currentTime = 0;
+          playNext();
+          audio.play();
+          timeout = null;
+        }, 360000);
+      });
+    }
+  
+  });
+  
